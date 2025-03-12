04ddb04fde3d50ced26e694e04e29e4e19cdd02b
orsel <orselv99@gmail.com>
2025-03-13 00:28:50 +0900

    프로젝트 초기화

---
+++ src/renderer/index.tsx
@@ -0,0 +1,6 @@
+import react from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+const root = createRoot(document.body);
+root.render(<h2>Hello from React!</h2>);
